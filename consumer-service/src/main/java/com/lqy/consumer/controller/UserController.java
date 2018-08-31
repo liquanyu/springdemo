4e435d079156d5250bdfbc86899ecd0a18376ba5
liquanyu <liquanyu@lymsh.com>
2018-08-31 16:28:52 +0800

    通过版本

--- consumer-service/src/main/java/com/lqy/consumer/controller/UserController.java
+++ consumer-service/src/main/java/com/lqy/consumer/controller/UserController.java
@@ -3,21 +3,27 @@ package com.lqy.consumer.controller;
 
 import com.lqy.consumer.model.OrderModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @RestController
 public class UserController {
 
-//    @Autowired
-//    private DiscoveryClient discoveryClient;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @Autowired
     private RestTemplate restTemplate;
-//    @Autowired
-//    private LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
 
     @GetMapping("/getUserOrder/{id}")
     public OrderModel getUserOrder(@PathVariable Integer id)
@@ -25,6 +31,16 @@ public class UserController {
         String url = "http://provider-service/getOrderById/"+id;
         OrderModel user = restTemplate.getForObject(url,OrderModel.class);
         return  user;
+    }
+
+    @GetMapping("/getRibbon")
+    public void getRibbon()
+    {
+        List<ServiceInstance> list = discoveryClient.getInstances("provider-service");
+        for (ServiceInstance si : list)
+        {
+            System.out.println(si.getHost()+":"+si.getPort());
+        }
     }
 
 }
