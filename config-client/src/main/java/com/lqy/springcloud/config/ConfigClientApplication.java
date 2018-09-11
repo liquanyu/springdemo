54e452c487cb244ce80e32ed5a98d1b2a39ca01e
liquanyu <liquanyu@lymsh.com>
2018-09-11 16:47:46 +0800

    update

--- config-client/src/main/java/com/lqy/springcloud/config/ConfigClientApplication.java
+++ config-client/src/main/java/com/lqy/springcloud/config/ConfigClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
